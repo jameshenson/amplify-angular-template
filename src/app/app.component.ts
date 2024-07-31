b1efb6ec30c6c8a2bdf663fcb15572087cbb2b3d
jameshenson <james.c.henson@gmail.com>
2024-07-31 15:00:24 -0400

    successfully added material slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,44 @@ import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
 
+// material componenets
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatMenuModule} from '@angular/material/menu';
+import { MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import {  MatDialogModule } from "@angular/material/dialog";
+import {  MatInputModule } from "@angular/material/input";
+import {  MatListModule } from "@angular/material/list";
+import {  MatPaginatorModule } from "@angular/material/paginator";
+import {  MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import {  MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSortModule } from "@angular/material/sort";
+import {  MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import { ReactiveFormsModule} from "@angular/forms";
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { MatRadioModule} from '@angular/material/radio';
+import {MatNativeDateModule} from '@angular/material/core';
+import { MatSliderModule} from '@angular/material/slider';
+import { MatTooltipModule} from '@angular/material/tooltip';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTreeModule} from '@angular/material/tree';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+
+
+
+
+
+
+
+
 Amplify.configure(outputs);
 
 @Component({
@@ -12,7 +50,40 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule],
+  imports: [RouterOutlet, 
+      TodosComponent, 
+      AmplifyAuthenticatorModule, 
+      MatSliderModule,
+    /*  MatSidenavModule,
+      BrowserAnimationsModule, 
+      MatMenuModule,
+      MatButtonModule,
+      MatIconModule,
+      MatCardModule,
+      MatTabsModule,
+      MatDatepickerModule,
+      MatDialogModule,
+      MatInputModule,
+      MatListModule,
+      MatPaginatorModule,
+      MatProgressSpinnerModule,
+      MatSelectModule,
+      MatSidenavModule,
+      MatSortModule,
+      MatTableModule,
+      MatToolbarModule,      
+      ReactiveFormsModule,
+      MatStepperModule,
+      MatCheckboxModule,
+      MatRadioModule,
+      MatNativeDateModule,
+      MatSliderModule,
+      MatTooltipModule,
+      DragDropModule,
+      MatGridListModule,
+      MatTreeModule,
+      ScrollingModule, */
+    ],
   
 })
 
